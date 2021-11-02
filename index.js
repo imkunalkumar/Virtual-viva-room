bcb06fecd649cc05b0f3d7787f0bc1b5bd62c8e4
Kaustubh Murumkar <kaustubh.murumkar@gmail.com>
2021-11-02 10:09:26 +0530

    Changes for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require("express")();
 const server = require("http").createServer(app);
+const compression = require('compression')
 const cors = require("cors");
+const path = require('path')
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -10,12 +12,19 @@ const io = require("socket.io")(server, {
 });
 
 app.use(cors());
+app.use(compression());
+
+app.use(express.static(path.resolve(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-	res.send('Running');
+app.get('/ping', (req, res) => {
+	res.send('pong');
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 io.on("connection", (socket) => {
 	socket.emit("me", socket.id);
